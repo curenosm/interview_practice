6eb03cd74c357ebbbb39e25e10c7e606e67dc4f4
Misael Cureño <curenosm@gmail.com>
2020-05-16 09:58:10 -0500

    Descriptions of the problem were added to each java file

--- java/Arrays/firstDuplicate.java
+++ java/Arrays/firstDuplicate.java
@@ -1,3 +1,22 @@
+/*
+    Given an array a that contains only numbers in the range from 
+    1 to a.length, find the first duplicate number for which the 
+    second occurrence has the minimal index. In other words, if there 
+    are more than 1 duplicated numbers, return the number for which 
+    the second occurrence has a smaller index than the second occurrence 
+    of the other number does. If there are no such elements, return -1.
+    
+    
+
+    [execution time limit] 4 seconds (py3)
+
+    [input] array.integer a
+
+    Guaranteed constraints:
+    1 ≤ a.length ≤ 105,
+    1 ≤ a[i] ≤ a.length.
+*/
+
 int firstDuplicate(int[] a) {
     // Recorre cada elemento del array
     for(int i = 0; i < a.length; i++){
--- java/LinkedLists/removeKFromList.java
+++ java/LinkedLists/removeKFromList.java
@@ -7,6 +7,16 @@
 //   ListNode<T> next;
 // }
 //
+
+/*
+    Note: Try to solve this task in O(n) time using O(1) additional space,
+    where n is the number of elements in the list, since this is what
+    you'll be asked to do during an interview.
+
+    Given a singly linked list of integers l and an integer k, remove
+    all elements from list l that have a value equal to k.
+*/
+
 ListNode<Integer> removeKFromList(ListNode<Integer> l, int k) {
     ListNode<Integer> newHead = new ListNode<>(null);
     newHead.next = l;
