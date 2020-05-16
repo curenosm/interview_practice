1768a51cce2bd0f83f4faaee9d030d04bbe031fd
Misael Cureño <curenosm@gmail.com>
2020-05-15 20:57:01 -0500

    tabs changed to spaces

--- python/Arrays/firstDuplicate.py
+++ python/Arrays/firstDuplicate.py
@@ -1,27 +1,27 @@
 def firstDuplicate(a):
-	"""
-	Given an array a that contains only numbers in the range from 
-	1 to a.length, find the first duplicate number for which the 
-	second occurrence has the minimal index. In other words, if there 
-	are more than 1 duplicated numbers, return the number for which 
-	the second occurrence has a smaller index than the second occurrence 
-	of the other number does. If there are no such elements, return -1.
-	
-	
+    """
+    Given an array a that contains only numbers in the range from 
+    1 to a.length, find the first duplicate number for which the 
+    second occurrence has the minimal index. In other words, if there 
+    are more than 1 duplicated numbers, return the number for which 
+    the second occurrence has a smaller index than the second occurrence 
+    of the other number does. If there are no such elements, return -1.
+    
+    
 
-	[execution time limit] 4 seconds (py3)
+    [execution time limit] 4 seconds (py3)
 
-	[input] array.integer a
+    [input] array.integer a
 
-	Guaranteed constraints:
-	1 ≤ a.length ≤ 105,
-	1 ≤ a[i] ≤ a.length.
+    Guaranteed constraints:
+    1 ≤ a.length ≤ 105,
+    1 ≤ a[i] ≤ a.length.
 
-	"""
-	conjunto = set()
-	for numero in lista:
-		if numero in conjunto:
-			return numero
-		conjunto.add(numero)
-	return -1
-	
+    """
+    conjunto = set()
+    for numero in lista:
+        if numero in conjunto:
+            return numero
+        conjunto.add(numero)
+    return -1
+    
--- python/LinkedLists/removeKFromList.py
+++ python/LinkedLists/removeKFromList.py
@@ -6,23 +6,23 @@
 #
 
 def removeKFromList(l, k):
-	"""
-	Note: Try to solve this task in O(n) time using O(1) additional space,
-	where n is the number of elements in the list, since this is what
-	you'll be asked to do during an interview.
+    """
+    Note: Try to solve this task in O(n) time using O(1) additional space,
+    where n is the number of elements in the list, since this is what
+    you'll be asked to do during an interview.
 
-	Given a singly linked list of integers l and an integer k, remove
-	all elements from list l that have a value equal to k.
-	"""
+    Given a singly linked list of integers l and an integer k, remove
+    all elements from list l that have a value equal to k.
+    """
 
     res = []
     while l:
-		# Obten el valor actual en la lista
+        # Obten el valor actual en la lista
         cur = l.value
-		# Si se queda entonces agregalo al resultado
+        # Si se queda entonces agregalo al resultado
         if cur != k:
             res.append(cur)
-		# Si no, pasate al siguiente
+        # Si no, pasate al siguiente
         l = l.next
     return res
 
@@ -30,20 +30,20 @@ def removeKFromList(l, k):
 
 # Otra solucion
 def removeKFromList(l, k):
-	# Apuntan al mismo objeto, pues no se hizo deepcopy
+    # Apuntan al mismo objeto, pues no se hizo deepcopy
     c = l
-	# Mientras la lista no haya terminado
+    # Mientras la lista no haya terminado
     while c:
-		# Si la lista aun no termina y el proximo valor es k
+        # Si la lista aun no termina y el proximo valor es k
         if c.next and c.next.value == k:
-			# Saltate el que sigue
+            # Saltate el que sigue
             c.next = c.next.next
         else:
-			# Si no, simplemente continua normal
+            # Si no, simplemente continua normal
             c = c.next
-	
-		# Como existe la posibilidad de el primer elemento
-		# fuese k y no se eliminó, pues se consideró siempre el
-		# siguiente, imprime desde el segundo si es k, si no
-		# imprime la lista entera (con el primer elemento)
-		return l.next if l and l.value == k else l
+    
+        # Como existe la posibilidad de el primer elemento
+        # fuese k y no se eliminó, pues se consideró siempre el
+        # siguiente, imprime desde el segundo si es k, si no
+        # imprime la lista entera (con el primer elemento)
+        return l.next if l and l.value == k else l
